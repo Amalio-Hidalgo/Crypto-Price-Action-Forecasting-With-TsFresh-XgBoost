0b3cfa9ded810db0f30972e87a8ab8f9b7ba7de2
Amalio Hidalgo Pickrell <amaliohidalgo1@gmail.com>
2025-02-09 17:15:22 +0100

    Add files via upload

--- My_FE_Wraps.py
+++ My_FE_Wraps.py
@@ -36,12 +36,12 @@ import multiprocessing
 #     Notes:
 #         - Optimized for distributed processing
 #         - Uses partition-wise rolling calculations
-def EF_Dask(df, ParameterComplexity=1, splits=5):
+def EF_Dask(df, ParameterComplexity=1, splits=5, target='prices ethereum', LR=False, Vol=False, forecast_periods=1,):
     if ParameterComplexity ==0: FC_parameters= MinimalFCParameters()
     elif ParameterComplexity ==1: FC_parameters= EfficientFCParameters()
     else: FC_parameters= ComprehensiveFCParameters()
-    meta= {'datetime': 'string', 'variable': 'string', 'value': 'float', 'id': 'string'}
-    func=  lambda x: roll_time_series(x, column_id='variable', column_sort='datetime', n_jobs=1).ffill()
+    meta= {'datetime': 'string', 'variable': 'string', 'value': 'float64', 'id': 'string'}
+    func=  lambda x: roll_time_series(x, column_id='variable', column_sort='datetime', n_jobs=1, chunksize=None).ffill()
     rolled= df.map_partitions(func, meta = meta)
     rolled= rolled.astype(meta)
     dates =rolled[['datetime','id']].groupby('id').last()
@@ -49,11 +49,18 @@ def EF_Dask(df, ParameterComplexity=1, splits=5):
     rolled_grouped = rolled.groupby(['id','variable'])
     features= dask_feature_extraction_on_chunk(rolled_grouped, column_id='id', column_sort='datetime',
                                                 column_kind='variable', column_value='value', default_fc_parameters= FC_parameters)
-    features= features.reset_index(drop=True).astype({'variable': 'string', 'value': 'float', 'id': 'string'})
+    features= features.reset_index(drop=True).astype({'variable': 'string', 'value': 'float64', 'id': 'string'})
     features= features.join(dates, how='left', on='id')
     features= dd.concat([features, concat], axis=0).drop('id', axis=1)
     features= features.categorize('variable')
     features= features.pivot_table(index='datetime', columns= 'variable', values='value')
+    features['LR(%)']= np.log(features[target])- np.log(features[target].shift(1))
+    features['LR(%)']= features['LR(%)']*100
+    features['1PeriodVol(%)']= abs(features['LR(%)'])
+    if LR== True: target_var= features['LR(%)']
+    elif Vol== True: target_var= features['1PeriodVol(%)']
+    else: target_var= features[target]
+    features['y_future']= target_var.shift(-forecast_periods)
     features=features.repartition(npartitions=splits)
     return features
 
